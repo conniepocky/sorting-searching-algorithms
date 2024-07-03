cfc87356a936e7f8107d029eed1924c18b38167d
Connie <c.amer@icloud.com>
2024-07-03 16:37:33 +0100

    recursive java binary search

--- binarysearch.java
+++ binarysearch.java
@@ -20,12 +20,30 @@ public class binarysearch {
         
         return -1;
     }
+
+    public static int bSearch(int arr[], int low, int high, int target) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] > target) {
+                return bSearch(arr, low, mid-1, target);
+            } else {
+                return bSearch(arr, mid+1, high, target);
+            }
+        }
+
+        return -1;
+    }
     
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int target = 3;
         
-        int result = binarySearch(arr, target);
+        int result = bSearch(arr, 0, arr.length-1, target);
         
         if (result == -1) {
             System.out.println("Element not found in the array");
